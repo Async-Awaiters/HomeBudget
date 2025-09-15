889a7f7489b14b12be106e9c3c2704db5aa22243
T1m-cpp <lehak1999@mail.ru>
2025-09-15 22:36:05 +0300

    Добавил сервис HealthCheck; мелкте правки

---
+++ HomeBudget.HealthCheck/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Подключаем HealthChecks и UI из appsettings.json
+builder.Services
+    .AddHealthChecksUI(options =>
+    {
+        options.SetEvaluationTimeInSeconds(
+            builder.Configuration.GetValue<int>("HealthChecksUI:EvaluationTimeInSeconds", 10));
+        options.SetMinimumSecondsBetweenFailureNotifications(
+            builder.Configuration.GetValue<int>("HealthChecksUI:MinimumSecondsBetweenFailureNotifications", 20));
+    })
+    .AddInMemoryStorage();
+
+builder.Services.AddHealthChecks();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapHealthChecksUI(options =>
+{
+    options.UIPath = "/health-ui"; // UI доступен по адресу http://localhost:5000/health-ui
+    options.ApiPath = "/health-api"; // JSON API для UI
+});
+
+app.Run();
